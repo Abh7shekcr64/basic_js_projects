e1b21a515279ed279e62479f852cf68eef899f2b
Abhishek <abh7shekcr64@gmail.com>
2025-06-16 10:30:28 +0530

    modified html file by adding some code into function

--- cricketgame/javascript/script.js
+++ cricketgame/javascript/script.js
@@ -1,17 +1,62 @@
 let compChoice;
-function generateComputerChoice () {
+function generateComputerChoice() {
      let randomNo = Math.random() * 3;
-      compChoice ;
+      //compChoice ;
     if( randomNo > 0 && randomNo <=1){
-    compChoice = 'Bat';
+    return compChoice = 'Bat';
     //console.log('computer choice is '+compChoice);
     }
     else if (randomNo > 1 && randomNo <= 2){
-    compChoice ='Ball';
+    return compChoice ='Ball';
       //  console.log('computer choice is '+compChoice);
     }
     else { 
-        compChoice= 'Stump';
+      return  compChoice= 'Stump';
       //  console.log('computer choice is '+compChoice);
     } 
+}
+
+function getResult(userMove, compMove ){
+  if(userMove ==='Bat'){
+    if(compMove ==='Bat'){
+      return  'It`s a Tie';
+    }
+
+    else if(compMove === 'Ball'){
+      return 'User Won.';
+    }
+
+    else{
+      return 'Computer Won.';
+    }
+  }
+
+  else if(userMove === 'Ball'){
+    if(compChoice === 'Ball'){
+      return 'It`s a Tie.';
+    }
+
+    else if(compMove === 'Bat'){
+      return 'Computer Won.';
+    }
+
+    else{
+      return 'User Won.';
+    }
+
+  }
+  else if(userMove === 'Stump'){
+    if(compChoice === 'Ball'){
+      return 'Computer Won.';
+    }
+
+    else if(compMove === 'Bat'){
+      return 'User Won.';
+    }
+
+    else {
+      return 'It`s a Tie';
+    }
+    }
+  
 }
